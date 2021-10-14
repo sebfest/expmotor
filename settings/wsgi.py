2f9b87e511b69b12107a4a037a7b100f552c8d49
sebastian <sebastian.fest@gmail.com>
2021-10-15 00:28:12 +0200

    Added pythonpath for apps.

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -8,8 +8,21 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+paths = [
+    os.path.join(BASE_DIR, 'apps'),
+]
+
+for index, path in enumerate(paths):
+    if path not in sys.path:
+        sys.path.insert(index, path)
+
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.production'
 
