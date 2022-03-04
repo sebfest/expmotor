24b1a0fa4b2ff07eb810c1c850d8a31b3640724e
sebastian <sebastian.fest@gmail.com>
2022-03-04 11:24:20 +0100

    Simplified settings.

--- settings/local.py
+++ settings/local.py
@@ -1,20 +1,85 @@
-from settings.base import *
+import os
+from django.core.exceptions import ImproperlyConfigured
 
+
+def get_env_variable(var_name):
+    """Get the environment variable or return exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = f'Set the {var_name} environment, variable'
+        raise ImproperlyConfigured(error_msg)
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
 INTERNAL_IPS = ['127.0.0.1']
 ALLOWED_HOSTS = INTERNAL_IPS
+SECRET_KEY = get_env_variable('DB_SECRET_KEY')
 
-# Email configuration to write on disk
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'messages')
+INSTALLED_APPS = [
+    'experiment.apps.ExperimentConfig',
+    'account.apps.AccountConfig',
+    'registration',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'bootstrap4',
+    'bootstrap_datepicker_plus',
+    'debug_toolbar',
+]
 
-# Debug toolbar
-INSTALLED_APPS += ['debug_toolbar']
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
 
-# Debug toolbar middleware
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+ROOT_URLCONF = 'settings.urls'
 
-# Database
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'apps', 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'settings.wsgi.application'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 POSTGRES_PW = get_env_variable('POSTGRES_PW')
 DATABASES = {
     'default': {
@@ -26,3 +91,40 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+AUTH_USER_MODEL = 'auth.User'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Registration settings
+ADMINS = [('sebastian', 'sebastian.fest@nhh.no')]
+LOGIN_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+REGISTRATION_OPEN = True
+REGISTRATION_DEFAULT_FROM_EMAIL = 'thechoicelab@nhh.no'
+REGISTRATION_ADMINS = ADMINS
+INCLUDE_REGISTER_URL = False
+INCLUDE_AUTH_URLS = True
+ACCOUNT_ACTIVATION_DAYS = 3
+
+# Django Sites
+SITE_ID = 1
+
+# Email configuration to write on disk
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'messages')
+
+
--- settings/production.py
+++ settings/production.py
@@ -1,16 +1,135 @@
+import os
 import dj_database_url
+from django.core.exceptions import ImproperlyConfigured
 
-from settings.base import *
 
+def get_env_variable(var_name):
+    """Get the environment variable or return exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = f'Set the {var_name} environment, variable'
+        raise ImproperlyConfigured(error_msg)
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = get_env_variable('DB_SECRET_KEY')
 DEBUG = False
-ALLOWED_HOSTS = ['expmotor.herokuapp.com', '*']
+ALLOWED_HOSTS = [
+    'expmotor.herokuapp.com',
+    '*',
+]
+
+INSTALLED_APPS = [
+    'experiment.apps.ExperimentConfig',
+    'account.apps.AccountConfig',
+    'registration',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'bootstrap4',
+    'bootstrap_datepicker_plus',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'settings.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'apps', 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'settings.wsgi.application'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 # Database configuration
+POSTGRES_PW = get_env_variable('POSTGRES_PW')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'expmotor',
+        'USER': 'admin',
+        'PASSWORD': POSTGRES_PW,
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 url = dj_database_url.config(default='postgres://...')
 DATABASES['default'].update(url)
 
-# Staticfile collection
+AUTH_USER_MODEL = 'auth.User'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LANGUAGE_CODE = 'en-us'
+
+# Time
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Registration
+ADMINS = [('sebastian', 'sebastian.fest@nhh.no')]
+LOGIN_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+REGISTRATION_OPEN = True
+REGISTRATION_DEFAULT_FROM_EMAIL = 'thechoicelab@nhh.no'
+REGISTRATION_ADMINS = ADMINS
+INCLUDE_REGISTER_URL = False
+INCLUDE_AUTH_URLS = True
+ACCOUNT_ACTIVATION_DAYS = 3
+
+# Sites
+SITE_ID = 1
 
 # Email configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
@@ -23,7 +142,7 @@ EMAIL_SUBJECT_PREFIX = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
 
-#SSL
+# SSL
 SECURE_SSL_REDIRECT = True
 
 
