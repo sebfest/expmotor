c9b00b3158ea10f6a44bc6ed38e460975b1e33bb
sebfest <sebastian.fest@gmail.com>
2024-06-11 22:36:15 +0200

    Added functionally to command.

--- apps/experiment/management/commands/generate_fake_data.py
+++ apps/experiment/management/commands/generate_fake_data.py
@@ -1,15 +1,20 @@
-from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
 from experiment.factories import ExperimentFactory
 
 
 class Command(BaseCommand):
     help = "Populate the database with fake data."
 
-    def handle(self, *args, **options):
-        self.stdout.write("Generating fake data...")
-        admin = User.objects.get(is_superuser=True)
-        ExperimentFactory.create_batch(size=5, manager=admin)
-        ExperimentFactory.create_batch(size=5)
-        self.stdout.write("...finished")
+    def add_arguments(self, parser):
+        parser.add_argument('--total', type=int, nargs='?', const=5, default=5,
+                            help='Indicates the number of experiments to be created')
 
+    def handle(self, *args, **kwargs):
+        total = kwargs['total']
+
+        self.stdout.write(self.style.WARNING("Generating {} fake experiments...".format(total)))
+        admin = User.objects.get(is_superuser=True)
+        ExperimentFactory.create_batch(size=total, manager=admin)
+        self.stdout.write(self.style.SUCCESS("...finished"))
