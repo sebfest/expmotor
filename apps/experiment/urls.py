fa6268247f3193bf1e205971d523297c98b9da75
sebfest <sebastian.fest@gmail.com>
2024-06-12 23:26:22 +0200

    Simplified URL pattern.

--- apps/experiment/urls.py
+++ apps/experiment/urls.py
@@ -10,97 +10,97 @@ urlpatterns = [
         name='experiment_list'
     ),
     path(
-        'experiment/add/',
+        'add/',
         views.ExperimentCreateView.as_view(),
         name='experiment_add'
     ),
     path(
-        'experiment/<int:pk>/',
+        '<int:pk>/',
         views.ExperimentDetailView.as_view(),
         name='experiment_detail'
     ),
     path(
-        'experiment/<int:pk>/update/',
+        '<int:pk>/update/',
         views.ExperimentUpdateView.as_view(),
         name='experiment_update'
     ),
     path(
-        'experiment/<int:pk>/delete/',
+        '<int:pk>/delete/',
         views.ExperimentDeleteView.as_view(),
         name='experiment_delete'
     ),
     path(
-        'experiment/<int:pk>/qrcode/',
+        '<int:pk>/qrcode/',
         views.ExperimentQrcodeDownloadView.as_view(),
         name='experiment_qr_download'
     ),
     path(
-        'experiment/<int:pk>/printout/',
+        '<int:pk>/printout/',
         views.ExperimentPrintoutDownloadView.as_view(),
         name='experiment_printout_download'
     ),
     path(
-        'experiment/<int:pk>/session/add/',
+        '<int:pk>/session/add/',
         views.SessionCreateView.as_view(),
         name='session_add'
     ),
     path(
-        'experiment/<int:pk_eks>/session/<int:pk>/',
+        '<int:pk_eks>/session/<int:pk>/',
         views.SessionDetailView.as_view(),
         name='session_detail'
     ),
     path(
-        'experiment/<int:pk_eks>/session/<int:pk>/update/',
+        '<int:pk_eks>/session/<int:pk>/update/',
         views.SessionUpdateView.as_view(),
         name='session_update'
     ),
     path(
-        'experiment/<int:pk_eks>/session/<int:pk>/delete/',
+        '<int:pk_eks>/session/<int:pk>/delete/',
         views.SessionDeleteView.as_view(),
         name='session_delete'
     ),
     path(
-        'experiment/<int:pk>/registrations/',
+        '<int:pk>/registrations/',
         views.RegistrationListView.as_view(),
         name='registration_list'
     ),
     path(
-        'experiment/<int:pk>/registrations/search/',
+        '<int:pk>/registrations/search/',
         views.RegistrationSearchView.as_view(),
         name='registration_search'
     ),
     path(
-        'experiment/<int:pk_eks>/session/<int:pk>/registration/add/',
+        '<int:pk_eks>/session/<int:pk>/registration/add/',
         views.RegistrationCreateView.as_view(),
         name='registration_add'
     ),
     path(
-        'experiment/<int:pk_eks>/session/<int:pk_ses>/registration/<int:pk>/update/',
+        '<int:pk_eks>/session/<int:pk_ses>/registration/<int:pk>/update/',
         views.RegistrationUpdateView.as_view(),
         name='registration_update'
     ),
     path(
-        'experiment/<int:pk_eks>/session/<int:pk_ses>/registration/<int:pk>/delete/',
+        '<int:pk_eks>/session/<int:pk_ses>/registration/<int:pk>/delete/',
         views.RegistrationDeleteView.as_view(),
         name='registration_delete'
     ),
     path(
-        'experiment/<int:pk>/register/',
+        '<int:pk>/register/',
         views.RegistrationView.as_view(),
         name='registration_create'
     ),
     path(
-        'experiment/<int:pk>/register/success/',
+        '<int:pk>/register/success/',
         views.RegistrationPreConfirmView.as_view(),
         name='registration_success'
     ),
     re_path(
-        r'^experiment/register/activate/(?P<uidb64>[\dA-Za-z_\-]+)/(?P<token>[\dA-Za-z]{1,6}-[\dA-Za-z]{1,32})/$',
+        r'^register/activate/(?P<uidb64>[\dA-Za-z_\-]+)/(?P<token>[\dA-Za-z]{1,6}-[\dA-Za-z]{1,32})/$',
         views.RegistrationActivateView.as_view(),
         name='registration_activate'
     ),
     path(
-        'experiment/register/confirm/',
+        'register/confirm/',
         views.RegistrationPostConfirmView.as_view(),
         name='registration_confirm'
     ),
