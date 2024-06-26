a133fff06ebaf37463d918a989483287f46c10b5
sebfest <sebastian.fest@gmail.com>
2024-06-26 12:34:48 +0200

    Switched off model signals for creation.

--- apps/experiment/management/commands/generate_fake_data.py
+++ apps/experiment/management/commands/generate_fake_data.py
@@ -1,7 +1,14 @@
+import factory
+
+
 from django.contrib.auth.models import User
+
 from django.core.management.base import BaseCommand
+from django.db.models import signals
 
 from experiment.factories import ExperimentFactory
+
+
 
 
 class Command(BaseCommand):
@@ -14,7 +21,8 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         total = kwargs['total']
 
-        self.stdout.write(self.style.WARNING("Generating {} fake experiments...".format(total)))
-        admin = User.objects.get(is_superuser=True)
-        ExperimentFactory.create_batch(size=total, manager=admin)
-        self.stdout.write(self.style.SUCCESS("...finished"))
+        with factory.django.mute_signals(signals.pre_save, signals.post_save):
+            self.stdout.write(self.style.WARNING("Generating {} fake experiments...".format(total)))
+            admin = User.objects.get(is_superuser=True)
+            ExperimentFactory.create_batch(size=total, manager=admin)
+            self.stdout.write(self.style.SUCCESS("...finished"))
