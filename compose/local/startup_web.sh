59f6b81cbf603a83709409e21f2b602ca0753a8a
sebfest <sebastian.fest@gmail.com>
2024-06-26 10:21:43 +0200

    Set verbosity level.

--- compose/local/startup_web.sh
+++ compose/local/startup_web.sh
@@ -17,7 +17,7 @@ python manage.py createsuperuserwithpassword \
         --email admin@example.org \
         --preserve
 python manage.py generate_fake_data
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input --clear -v 0
 gunicorn settings.wsgi:application --bind 0.0.0.0:8000 --reload
 
 exec "$@"
