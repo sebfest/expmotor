586caba833b0954f66dd7abdadbc422178ee4534
sebfest <sebastian.fest@gmail.com>
2024-08-14 10:33:07 +0200

    Logging to stdout

--- compose/production/startup_web.sh
+++ compose/production/startup_web.sh
@@ -2,8 +2,16 @@
 
 wait-for-it "$POSTGRES_HOST":"$POSTGRES_PORT"
 
-#python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input --clear
+python manage.py flush --no-input
+python manage.py migrate
 
-gunicorn settings.wsgi:application --bind 0.0.0.0:8000 --forwarded-allow-ips="*" --proxy-allow-from="*"
+gunicorn settings.wsgi:application \
+	--bind 0.0.0.0:8000 \
+	--access-logfile "-" \
+	--error-logfile "-" \
+	--forwarded-allow-ips="*" \
+	--proxy-allow-from="*" \
+	--env "SCRIPT_NAME=/app"
 
 exec "$@"
