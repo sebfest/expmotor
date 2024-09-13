abe1580802bac6c8002afbd7a92421405ccffbc6
sebfest <sebastian.fest@gmail.com>
2024-09-13 11:36:36 +0200

    Change to pathlib.

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -1,17 +1,17 @@
-import os
 import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 paths = [
     BASE_DIR,
-    os.path.join(BASE_DIR, 'apps'),
+    BASE_DIR / 'apps',
 ]
 
 for index, path in enumerate(paths):
-    if path not in sys.path:
-        sys.path.insert(index, path)
+    if str(path) not in sys.path:
+        sys.path.insert(index, str(path))
 
 application = get_wsgi_application()
